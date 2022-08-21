28e9ec9daf632eed7f0faf8fdfb6b1cd5a9ad080
«Slavyanus <slavyanus@inbox.ru>
2022-08-22 03:49:29 +0700

    summa

---
+++ z2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+using System;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random(DateTime.Now.Millisecond);
+        int[] msv = (new int[10]).Select(i => rnd.Next(-5, 6)).ToArray();
+        Console.WriteLine("Исходный массив:");
+        foreach (int i in msv) Console.Write("{0} ", i);
+ 
+        var sum1 = msv.Where((n, i) => (i++ % 2) == 0).Sum();
+        Console.Write("\n\nCуммa элементов массива с нечетными номерами: {0}\n\n", sum1);
+ 
+        
+    }
+}
