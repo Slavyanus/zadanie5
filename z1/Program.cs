2052c9354ab95803b18733d586515011450df4fd
«Slavyanus <slavyanus@inbox.ru>
2022-08-22 03:48:19 +0700

    massiv +999

---
+++ z1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+class Program
+{
+static void Main(string[] args)
+{
+int[] mas = new int[400];
+Random r = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+mas[i] = r.Next(100, 999);
+Console.Write(mas[i] + " ");
+}
+Console.Read();
+
+
+
+}
+}
+}
