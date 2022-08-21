0cacc18f0c3a232acedfc4272e10dbad4a4f7d62
«Slavyanus <slavyanus@inbox.ru>
2022-08-22 03:51:53 +0700

    raznica

---
+++ z3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+
+
+    using System;
+    using System.Linq;
+     
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            double[] d = Enumerable.Repeat(0, 10).Select(x => rand.NextDouble()).ToArray();
+            Console.WriteLine("Массив d:");
+            Console.WriteLine(string.Join(" ", d.Select(x => x.ToString("F2")).ToArray()));
+            Console.WriteLine(new string('-', 25));
+            Console.WriteLine("Максимум в массиве d: " + d.Max().ToString("F2"));
+            Console.WriteLine("Минимум в массиве d: " + d.Min().ToString("F2"));
+            Console.WriteLine("Разность для массива d:" + (d.Max() - d.Min()).ToString("F2"));
+            Console.WriteLine(new string('-', 25));
+            Console.ReadKey(true);
+        }
+    }
